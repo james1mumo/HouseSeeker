71b740ef4de75e203572996e0ab3cdcf14c52360
james mumo <james1mumo@gmail.com>
2021-10-09 03:18:04 +0300

    registration

--- app/src/main/java/com/dekut/houseseeker/MainActivity.java
+++ app/src/main/java/com/dekut/houseseeker/MainActivity.java
@@ -2,7 +2,10 @@ package com.dekut.houseseeker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.dekut.houseseeker.ui.LandlordRegistrationActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(getBaseContext(), LandlordRegistrationActivity.class));
     }
 }
